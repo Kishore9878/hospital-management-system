c67bba686ddaaec08479056220f30dc6389d31b0
Kishore <gadulakishore2000@gmail.com>
2026-06-04 21:37:55 +0530

    updated backend production url

--- frontend/src/constant/Api.js
+++ frontend/src/constant/Api.js
@@ -1,3 +1,5 @@
 // export const backendApi = "https://hospital-web-backend.vercel.app"; // vercel
 // export const backendApi = "https://hospital-web-backend.onrender.com"; // render
-export const backendApi = "http://localhost:3060"; // local
+// export const backendApi = "http://localhost:3060"; // local
+
+export const backendApi = "https://hospital-management-system-ebhn.onrender.com";
