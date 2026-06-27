168e88ab3131e40f53b109802bc880f324dd7652
Kishore <gadulakishore2000@gmail.com>
2026-06-27 15:58:08 +0530

    made small change

--- frontend/src/constant/Api.js
+++ frontend/src/constant/Api.js
@@ -2,6 +2,6 @@
 // export const backendApi = "https://hospital-web-backend.onrender.com"; // render
 // export const backendApi = "http://localhost:3060"; // local
 
-export const backendApi = import.meta.env.VITE_BACKEND_URL 
-  ? import.meta.env.VITE_BACKEND_URL.replace(/\/api$/, "") 
-  : "https://hospital-management-system-ebhn.onrender.com";
+export const backendApi =
+  import.meta.env.VITE_BACKEND_URL?.replace(/\/api\/v1$/, "") ||
+  "https://hospital-management-system-8oc0.onrender.com";
